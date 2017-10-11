393c828727e756f317669913cb65976091b8e489
sseth <sahiilseth@gmail.com>
2017-10-11 13:57:55 -0500

    Update parse_bamreadcount.R

--- parse_bamreadcount.R
+++ parse_bamreadcount.R
@@ -30,15 +30,40 @@ bam_readcount.parseline <- function(x){
 
 # chr	position	reference_base	depth	base:count:avg_mapping_quality:avg_basequality:avg_se_mapping_quality:num_plus_strand:num_minus_strand:avg_pos_as_fraction:avg_num_mismatches_as_fraction:avg_sum_mismatch_qualities:num_q2_containing_reads:avg_distance_to_q2_start_in_q2_reads:avg_clipped_length:avg_distance_to_effective_3p_end   ...
 #x='/rsrch2/iacs/ngs_runs/1411_sarcomatoid/tmap/bams/tmp.bamreadcount'
-bam_readcount.parse <- function(x){
+bam_readcount.parse <- function(x, 
+                                samplename,
+                                bed){
   
   library(params)
   library(dplyr)
   
-  
   tmp = scan(x, what = "character", sep = "\n")
   
   dat = lapply(tmp, bam_readcount.parseline) %>% bind_rows()
+  dat2 = select(dat, chr, start = pos, ref_allele = ref, alt_allele = base, alt_count = count) %>% 
+    mutate(start = as.integer(start), 
+           end = start, 
+           alt_count = as.integer(alt_count), 
+           samplename = samplename) %>% 
+    group_by(chr, start) %>% 
+    mutate(dp = sum(alt_count), 
+           af = alt_count / dp) %>% 
+    ungroup()
+  
+  # add ref counts
+  dat_ref = select(dat2, chr, start, ref_allele, alt_allele, alt_count) %>% 
+    filter(ref_allele == alt_allele) %>% mutate(ref_count = alt_count) %>% 
+    select(-alt_count, -ref_allele, -alt_allele)
+  
+  dat2 = left_join(dat2, dat_ref, by = c("chr", "start"))
+  
+  # combine with bed
+  if(!is.data.frame(bed))
+    if(file.exists(bed))
+      bed = read_tsv(bed)
+  
+  dat3 = left_join(bed, dat2, by = c("chr", "start", "end", 
+                                     "ref_allele", "alt_allele"))
   
 
 }
